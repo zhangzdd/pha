10fc6c99ed8ce35f4a4aeb7dd057fec4e738891a
zhangzdd <69838968+zhangzdd@users.noreply.github.com>
2021-10-27 21:24:44 +0800

    Added scatter/line selection,with a broader view

--- new_design/logic.py
+++ new_design/logic.py
@@ -10,13 +10,21 @@ from PyQt5 import QtCore
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QSizePolicy, QFileDialog
 import pyqtgraph as pg
+from pyqtgraph import exporters
 import numpy as np
 from skimage.measure import block_reduce
 from client import receiver
 from threading import Thread
 import time
+from scipy import signal
 
-from brush import AddRegionWidget
+from brush import CurveWidget,ScatterWidget
+
+def clearLayout(layout):
+    while layout.count():
+        child = layout.takeAt(0)
+        if child.widget():
+            child.widget().deleteLater()
 
 class CommonHelper:
     def __init__(self):
@@ -33,8 +41,9 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         self.setupUi(self)
         self.collection_settings_save.clicked.connect(self.collections_setting_func)
         #self.graph = pg.PlotWidget(self)
-        self.divided_graph = AddRegionWidget(parent=None)
-        self.gridLayout.addWidget(self.divided_graph)
+        self.divided_graph_curve = CurveWidget(parent=None)
+        self.divided_graph_scatter = ScatterWidget(parent=None)
+        self.gridLayout.addWidget(self.divided_graph_curve)
         self.rawdata = []
         #self.binned = np.zeros(1024)
         self.buffer = []
@@ -46,6 +55,10 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         self.pause_button.clicked.connect(self.pause_collection)
         self.end_button.clicked.connect(self.end_collection)
         self.on_collection = False
+        self.status = "End"
+        self.end_option = "Time"
+        self.full_time = 0
+        self.full_count = 0
         
         self.disp = QtWidgets.QLabel()
         self.collection_setting_display.addWidget(self.disp)
@@ -59,12 +72,19 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         self.timer.timeout.connect(self.update_plot)
         #self.timer.start(1000)
 
+        self.global_time = 0
+        self.start_time = 0
+        self.end_time = 0
+
         self.plot_option = self.plot_method.currentText()
         self.expand_option = "Original"
         self.display_setting_save.clicked.connect(self.display_setting_func)
 
         self.source = "Simulation"
         #self.receiver_agent = receiver("127.0.0.1",6688,1024)
+
+        self.export_spectrum.clicked.connect(self.export_image)
+        self.set_path.clicked.connect(self.new_path)
 
         qssStyle = CommonHelper.readQss("qss/MacOS.qss")
         self.setStyleSheet(qssStyle)
@@ -73,19 +93,39 @@ class MainWindow(QMainWindow,Ui_MainWindow):
     def display_setting_func(self):
         self.plot_option = self.plot_method.currentText()
         self.expand_option = self.curve_expansion.currentText()
+        if self.plot_option == "Line":
+            clearLayout(self.gridLayout)
+            self.divided_graph_curve = CurveWidget(parent=None)
+            self.gridLayout.addWidget(self.divided_graph_curve)
+            print("Line show")
+        
+        elif self.plot_option == "Scatter":
+            clearLayout(self.gridLayout)
+            self.divided_graph_scatter = ScatterWidget(parent=None)
+            self.gridLayout.addWidget(self.divided_graph_scatter)
+            print("Scatter show")
+
 
 
     def collections_setting_func(self):
         print(self.collection_source.currentText())
-        print("Collection settings saved")
-        self.source = self.collection_source.currentText()
         
+        self.source = self.collection_source.currentText()
+        if self.count_time_option.isChecked():
+            self.end_option = "Time"
+            self.full_time = float(self.count_time_setting.text())
+            print("Expected time: {}".format(self.full_time))
+        elif self.total_count_option.isChecked():
+            self.end_option = "Count"
+            self.full_count = float(self.tota_count_setting.text())
+            print("Expected count: {}".format(self.full_count))
         
         #调用QtGui.QPixmap方法，打开一个图片，存放在变量中
         Icon = QtGui.QPixmap(sys.path[0]+'/templates/'+self.source+".jpeg")
         # 在disp里面，调用setPixmap命令，建立一个图像存放框，并将之前的图像png存放在这个框框里。
         self.disp.setPixmap(Icon)
         self.disp.setScaledContents(True)
+        print("Collection settings saved")
         pass
     
     def link(self):
@@ -141,10 +181,36 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         
 
     def update_data(self):
+        self.global_time = time.time() - self.start_time
+        
+        if self.end_option == "Time" and self.global_time > self.full_time:
+            print("Reached full time")
+            self.pause_collection()
+            return
+        elif self.end_option == "Count" and sum(self.original) > self.full_count:
+            print("Reached full count")
+            self.pause_collection()
+        
+        if self.end_option == "Time":
+            self.finished_percentage.setValue(int(self.global_time/self.full_time*100))
+        elif self.end_option == "Count":
+            self.finished_percentage.setValue(int(sum(self.original)/self.full_count*100))
+        
         if self.on_collection == False:
             return 
+        
+        self.count_rate.display(sum(self.original)/self.global_time)
+        self.calculate_integral()
+        self.calculate_peak_area()
+        self.calculate_half_wave()
+        
         if self.source == "Simulation":
             self.original = self.original*1.005
+            self.time_elapsed.display(self.global_time)
+            self.total_count.display(sum(self.original))
+            return
+        self.time_elapsed.display(self.global_time)
+        self.total_count.display(sum(self.buffer))
         print(max(self.buffer))
         """
         new_income = np.random.randn()
@@ -159,50 +225,101 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         if self.on_collection == False:
             return
         
+        rate = int(len(self.original)/1024)
         if self.expand_option == "Original":
             self.binned = self.original
             pass
         if self.expand_option == "Interval":
             length = len(self.original)
-            self.binned = self.original[0:length:4]
+            self.binned = self.original[0:length:rate]
         elif self.expand_option == "Max":
-            self.binned = block_reduce(self.original,(4,),np.max)
+            self.binned = block_reduce(self.original,(rate,),np.max)
             pass
         elif self.expand_option == "Mean":
-            self.binned = block_reduce(self.original,(4,),np.mean)
+            self.binned = block_reduce(self.original,(rate,),np.mean)
         
+
         if self.plot_option == "Line":
             #self.graph.clear()
             #self.curve1 = self.graph.plot(self.binned)
-            self.divided_graph.display_data_curve(self.binned)
+            #self.divided_graph = CurveWidget(parent=None)
+            #print("Line show")
+            self.divided_graph_curve.display_data_curve(self.binned)
         
         elif self.plot_option == "Scatter":
-            x = self.binned
-            y = np.array(range(0,len(self.binned)))
-
+            y = self.binned
+            x = np.array(range(0,len(self.binned)))
+            #self.divided_graph = ScatterWidget(parent=None)
             #self.graph.clear()
             #self.curve2 = self.graph.plot(y,x,pen=None,symbol="o")
-            self.divided_graph.display_data_scatter(y,x)
+            #print("Scatter show")
+            self.divided_graph_scatter.display_data_scatter(x,y)
 
+    def calculate_peak_area(self):
+        min_x,max_x = self.divided_graph_curve.region.getRegion()
+        area = sum(self.binned[int(min_x):int(max_x)] - min(self.binned[int(min_x):int(max_x)]))
+        self.peak_area.display(area)
+
+    def calculate_integral(self):
+        min_x,max_x = self.divided_graph_curve.region.getRegion()
+        area = sum(self.binned[int(min_x):int(max_x)])
+        self.integral.display(area)
+    
+    def calculate_half_wave(self):
+        min_x,max_x = self.divided_graph_curve.region.getRegion()
+        portion = self.binned[int(min_x):int(max_x)]
+        width = signal.peak_widths(self.binned,peaks=[np.argmax(portion)],rel_height=0.6)
+        print(width)
+        self.half_wave.display(width[2][0])
 
     def start_collection(self):
         print("Started")
+        if self.on_collection:
+            return 
         self.on_collection = True
-
-        self.timer.start(1000)
+        if self.status == "Pause":
+            #self.start_time = time.time()
+            print("Resume previous round")
+            pass
+        elif self.status == "End":
+            print("New round")
+            self.start_time = time.time()
+        self.status = "Start"
+        self.timer.start(500)
     
     def pause_collection(self):
         print("Paused")
         self.on_collection = False
+        self.end_time = time.time()
+        self.status = "Pause"
         self.timer.stop()
     
     def end_collection(self):
         print("Ended")
+        self.end_time = 0
+        self.start_time = 0
+        self.status = "End"
         self.on_collection = False
         self.rawdata = []
-        self.original = None
+        self.original = np.histogram(np.random.normal(size=40960),bins=4096)[0]
         self.timer.stop()
+    
+    def export_image(self):
+        self.pause_collection()
+        print("Save picture")
+        save_path = self.cur_path_display.text()
+        exporter = exporters.ImageExporter(self.divided_graph_curve.main_plotter)
 
+        filename = time.asctime(time.localtime(time.time())).replace(" ","_").replace(":","_")
+        #filename = "default"
+        exporter.parameters()['width'] = 1000
+        exporter.parameters()['height'] = 500
+        print("Save to "+save_path + filename + '.png')
+        exporter.export(save_path + filename + '.png')
+
+    def new_path(self):
+        save_path = QFileDialog.getExistingDirectory(None,"Select your path",self.cur_path_display.text())  
+        self.cur_path_display.setText(save_path+"/")
 
 if __name__ == "__main__":
     #固定的，PyQt5程序都需要QApplication对象。sys.argv是命令行参数列表，确保程序可以双击运行
